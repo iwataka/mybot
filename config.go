4395a7b79c5084edbd9508c90eb94ef4bbf80a47
iwataka <ap.gr.le@gmail.com>
2016-08-07 16:40:38 +0900

    Modify to use config.template.yml if no config.yml

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
@@ -52,7 +53,11 @@ type notificationConfig struct {
 
 func unmarshalConfig(path string) error {
 	if path == "" {
-		path = "config.yml"
+		if info, _ := os.Stat("config.yml"); info != nil && !info.IsDir() {
+			path = "config.yml"
+		} else {
+			path = "config.template.yml"
+		}
 	}
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
