602a68bb8075f4f7cb19ae7dee79535a9b1f7610
iwataka <ap.gr.le@gmail.com>
2016-09-10 04:46:09 +0000

    Enable to configure port number when using Docker

--- setup.sh
+++ setup.sh
@@ -12,7 +12,13 @@ else
     container_name=$MYBOT_CONTAINER_NAME
 fi
 
+if [[ -z $MYBOT_PORT ]]; then
+    port="8080"
+else
+    port=$MYBOT_PORT
+fi
+
 docker run -d -v "$dir":/mybot \
     --name "$container_name" \
-    -p 8080:8080 \
+    -p "$port":8080 \
     mybot sh -c "cd /mybot && go get -d ./... && go generate && go build && ./mybot s --log .mybot-debug.log >> .mybot-dump.log 2>&1"
