1497e2e59ffa539891b9d0491145c1f481f35a3f
iwataka <ap.gr.le@gmail.com>
2022-05-04 15:17:25 +0900

    fix: lint issue

--- main.go
+++ main.go
@@ -564,11 +564,11 @@ func serve(c *cli.Context) error {
 
 	// Wait for interrupt signal to gracefully shutdown the server with
 	// a timeout of 5 seconds.
-	quit := make(chan os.Signal)
+	quit := make(chan os.Signal, 1)
 	// kill (no param) default send syscall.SIGTERM
 	// kill -2 is syscall.SIGINT
 	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) //nolint:staticcheck
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	logger.Println("Shutting down server...")
 
